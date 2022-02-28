036504b46c6ec3365b487a1fedf7cf0925c0a6db
Emanuel Jarquin <48668590+ejarquin22@users.noreply.github.com>
2022-02-28 13:30:27 -0600

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,8 +1,54 @@
+
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+from collections import Counter
+import matplotlib.pyplot as plt
+
+from ejarquin22.linear_algebra import sum_of_squares
+from ejarquin22.linear_algebra import dot
+
+friend_counts = Counter(num_friends)
+xs = range(101)                         # largest value is 100
+ys = [friend_counts[x] for x in xs]     # height is just # of friends
+plt.bar(xs, ys)
+plt.axis([0, 101, 0, 25])
+plt.title("Histogram of Friend Counts")
+plt.xlabel("# of friends")
+plt.ylabel("# of people")
+# plt.show()
+
+num_points = len(num_friends)               # 204
+
+
+assert num_points == 204
+
+largest_value = max(num_friends)            # 100
+smallest_value = min(num_friends)           # 1
+
+
+assert largest_value == 100
+assert smallest_value == 1
+
+sorted_values = sorted(num_friends)
+smallest_value = sorted_values[0]           # 1
+second_smallest_value = sorted_values[1]    # 1
+second_largest_value = sorted_values[-2]    # 49
+
+
+assert smallest_value == 1
+assert second_smallest_value == 1
+assert second_largest_value == 49
+
+
 from typing import List
 
 def mean(xs: List[float]) -> float:
     return sum(xs) / len(xs)
 
+mean(num_friends)   # 7.333333
+
+
+assert 7.3333 < mean(num_friends) < 7.3334
 
 # The underscores indicate that these are "private" functions, as they're
 # intended to be called by our median function but not by other people
@@ -25,6 +71,18 @@ assert median([1, 10, 2, 9, 5]) == 5
 assert median([1, 9, 2, 10]) == (2 + 9) / 2
 
 
+assert median(num_friends) == 6
+
+def quantile(xs: List[float], p: float) -> float:
+    """Returns the pth-percentile value in x"""
+    p_index = int(p * len(xs))
+    return sorted(xs)[p_index]
+
+assert quantile(num_friends, 0.10) == 1
+assert quantile(num_friends, 0.25) == 3
+assert quantile(num_friends, 0.75) == 9
+assert quantile(num_friends, 0.90) == 13
+
 def mode(x: List[float]) -> List[float]:
     """Returns a list, since there might be more than one mode"""
     counts = Counter(x)
@@ -32,10 +90,7 @@ def mode(x: List[float]) -> List[float]:
     return [x_i for x_i, count in counts.items()
             if count == max_count]
 
-def quantile(xs: List[float], p: float) -> float:
-    """Returns the pth-percentile value in x"""
-    p_index = int(p * len(xs))
-    return sorted(xs)[p_index]
+assert set(mode(num_friends)) == {1, 6}
 
 # "range" already means something in Python, so we'll use a different name
 def data_range(xs: List[float]) -> float:
